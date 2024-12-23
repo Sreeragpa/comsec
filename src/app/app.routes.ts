401ee653274ed289b6307ed43952a8d14cace5cf
@ankur6042815 <ankur@synram.com>
2024-12-23 18:59:33 +0530

    user dashboard ui , add company ui login page compleated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
+import { MainFrameComponent } from './layout/main-frame/main-frame.component';
+import { AddCompanyComponent } from './pages/add-company/add-company.component';
 
 export const routes: Routes = [
     {
-        path:'',component:UserDashboardComponent
+        path:'',component:LoginComponent
     },
     {
         path:'login',component:LoginComponent
+    },
+    {
+        path:'user-dashboard',component:UserDashboardComponent
+    },
+    {
+        path:'add-company',component:AddCompanyComponent
     },
     
 ];
---
+++ src/app/layout/main-frame/main-frame.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-frame',
+  imports: [],
+  templateUrl: './main-frame.component.html',
+  styleUrl: './main-frame.component.css'
+})
+export class MainFrameComponent {
+
+}
